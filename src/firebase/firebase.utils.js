bf953c54a2f5331ac0f2971f92dea265ac0f688f
Brian <haack79@hotmail.com>
2019-08-28 07:26:33 -0700

    implemented firebase utils, auth and allow users to be added to db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,27 @@ const config = {
     appId: "1:777564370751:web:49ef9184e98237dd"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (e) {
+        console.log('error creating user', e.message);
+      }
+    }
+    return userRef;
+  }
 
   firebase.initializeApp(config);
 
