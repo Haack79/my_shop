d650af5c7d59dcfcb9bb4d92084862cb0c4206be
Brian <haack79@hotmail.com>
2019-09-04 07:27:10 -0700

    added carticon and dropdown for cartitems that can be toggled

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils.js';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import CartIcon from '../cart-icon/cart-icon.component';
 import { connect } from 'react-redux';
 import './header.styles.scss';
 
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -24,10 +26,13 @@ const Header = ({ currentUser }) => (
                 :
                 <Link className='option' to='/signin'>Sign In</Link>
             }
+            <CartIcon />
         </div>
+            { hidden ? null : <CartDropdown />}
     </div>
 );
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser 
+const mapStateToProps = ({user: {currentUser }, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header); 
