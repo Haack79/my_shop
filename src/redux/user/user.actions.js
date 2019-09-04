af563a487f68ac756dcc6f943cc36b25426655b9
Brian <haack79@hotmail.com>
2019-09-04 06:20:34 -0700

    updated for redirect on signin and signout added types for user

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,7 @@
 //functions that return objects/  the type must be the exact same as what reducer is expecting, payload is the state slice.
+import {UserActionTypes} from './user.types';
 
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
 })
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,12 +1,13 @@
 //function that gets object and aciton
 //object has a type: the action that's coming ;; and payload: to do something
 // state is what will come from the store, when app first initializes there is no state so need to set it up.
+import {UserActionTypes} from './user.types';
 const INITIAL_STATE = {
     currentUser: null
 }
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
