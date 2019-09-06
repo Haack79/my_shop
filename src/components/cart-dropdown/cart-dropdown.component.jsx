cec05e00615e95768e0346b14498b5513c26c469
Brian <haack79@hotmail.com>
2019-09-06 07:28:56 -0700

    added cart-item component and connected cart with dropdown and cartitems reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
-            <CustomButton>Go To Checkout</CustomButton>
+            {
+                cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem} />
+                ))
+            }
         </div>
+        <CustomButton>Go To Checkout</CustomButton>
     </div>
-)
-export default CartDropdown;
+);
+//this is pulling in state
+// const mapStateToProps = state => ({
+//     cartItems: state
+// })
+//so going to destructure items from object i need from state
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown);
