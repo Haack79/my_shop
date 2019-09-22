733bfa1d4b27ab891e27746cf2755c8d4944584f
Brian <haack79@hotmail.com>
2019-09-21 21:26:46 -0700

    optimized data with Data Normalization, and collection

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+import { Route } from 'react-router-dom';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+// import { connect } from 'react-redux';
+// import { createStructuredSelector } from 'reselect';
+// import SHOP_DATA from './shop.data.js';
+// import { selectCollections } from '../../redux/shop/shop.selectors';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            collections: SHOP_DATA
-            
-        }
-    }
+// import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+// changed from class to functional component since getting data stored from somewhere else. 
+const ShopPage = ({match}) => (
+    <div>
+        <Route exact path={`${match.path}`}component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))
-                    }
-            </div>
-        );
-    }
-}
+    </div>
+
+)
+
+// const mapStateToProps = createStructuredSelector({
+//     collections: selectCollections
+// });
 
 export default ShopPage; 
